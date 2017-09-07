25917d359972fa938f938e89bb661c226fd2477c
Krishnan Rajiyah <krajiyah@gmail.com>
2017-09-06 18:16:56 -0700

    finished step 5 corner cases

--- app.rb
+++ app.rb
@@ -61,17 +61,20 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
+    redirect "/win" if @game.check_win_or_lose == :win
+    redirect "/lose" if @game.check_win_or_lose == :lose
     erb :show # You may change/remove this line
   end
 
   get '/win' do
-    ### YOUR CODE HERE ###
+    redirect "/show" if @game.check_win_or_lose == :play
+    redirect "/lose" if @game.check_win_or_lose == :lose
     erb :win # You may change/remove this line
   end
 
   get '/lose' do
-    ### YOUR CODE HERE ###
+    redirect "/win" if @game.check_win_or_lose == :win
+    redirect "/show" if @game.check_win_or_lose == :play
     erb :lose # You may change/remove this line
   end
 
